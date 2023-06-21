8c94cc93574fc2e7010cb4ebb5c63948a4d90d8b
Kai Patel <00kaipatel@gmail.com>
2023-06-21 01:39:08 +0100

    feat: connect ListView to sent messages

--- app/src/main/java/duplex/AppController.java
+++ app/src/main/java/duplex/AppController.java
@@ -1,18 +1,35 @@
 package duplex;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class AppController {
+    public ObservableList<String> messages;
+    public ObservableList<String> users;
+
     public TextArea textArea;
     public Button sendButton;
 
+    public ListView<String> messageHistory;
+    public ListView<String> userList;
+
     public void sendMessage() {
-        System.out.println(textArea.getText());
+        String newMessage = textArea.getText();
+
+        if (newMessage.isBlank()) {
+            return;
+        }
+
+        System.out.println(newMessage);
+        messages.add(newMessage);
+        messageHistory.scrollTo(messages.size());
         textArea.clear();
     }
 
@@ -26,5 +43,13 @@ public class AppController {
         if (e.getCode() == KeyCode.ENTER && e.isControlDown()) {
             sendMessage();
         }
+    }
+
+    @FXML
+    public void initialize() {
+        messages = FXCollections.observableArrayList("this", "is", "a", "test");
+        users = FXCollections.observableArrayList("John", "Adam", "Steve");
+        messageHistory.setItems(messages);
+        userList.setItems(users);
     }
 }
