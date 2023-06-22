7c74b44fa22b0afa636c88f3895a23b4923270e5
Kai Patel <00kaipatel@gmail.com>
2023-06-22 19:39:01 +0100

    feat: Message fields and serialization

--- common/src/main/java/duplex/Message.java
+++ common/src/main/java/duplex/Message.java
@@ -1,5 +1,40 @@
 package duplex;
 
-public class Message {
+import java.io.Serializable;
+import java.util.Date;
 
+public class Message implements Serializable {
+    private String content;
+    private String sender;
+    private Date timeSent;
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timeSent = new Date();
+    }
+
+    public Date getTimeSent() {
+        return timeSent;
+    }
+
+    public void setTimeSent(Date timeSent) {
+        this.timeSent = timeSent;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
 }
