292352adaa376bb46d47f7c578827ffdd21d64b9
Kai Patel <00kaipatel@gmail.com>
2023-06-22 19:56:57 +0100

    fix: remove unused imports

--- server/src/main/java/duplex/ClientHandler.java
+++ server/src/main/java/duplex/ClientHandler.java
@@ -1,8 +1,6 @@
 package duplex;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
